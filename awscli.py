91f908399d064cd8d4529db7b72e35ba8d422945
darshan <dar.raul@gmail.com>
2019-05-01 01:45:18 +0530

    updated progress bar

--- awscli.py
+++ awscli.py
@@ -18,6 +18,9 @@ import boto3
 from time import sleep
 import sys
 
+from progress.bar import FillingCirclesBar
+
+
 ### initialize service clients #############
 s3 = boto3.client('s3')
 iam = boto3.client('iam')
@@ -28,12 +31,18 @@ f = Figlet(font='big')
 
 ################### progress bar function #########
 def progressbar():
-    for i in range(21):
-        sys.stdout.write('\r')
-        # the exact output you're looking for:
-        sys.stdout.write("[%-20s] %d%%" % ('='*i, 5*i))
-        sys.stdout.flush()
-        sleep(0.05)
+    # for i in range(21):
+    #     sys.stdout.write('\r')
+    #     # the exact output you're looking for:
+    #     sys.stdout.write("[%-20s] %d%%" % ('='*i, 5*i))
+    #     sys.stdout.flush()
+    #     sleep(0.05)
+    bar = FillingCirclesBar('Processing', max=100)
+    for i in range(100):
+        # Do some work
+        sleep(0.025)
+        bar.next()
+    bar.finish()
 
  ##########################getters#########################   
  # s3    
