c060c09a3098d8424ef556ae9a5a88284b992a13
darshan <dar.raul@gmail.com>
2019-05-04 23:17:39 +0530

    test

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -7,11 +7,19 @@ def test_getbuckets():
     buckets=s3.list_buckets()
     for i in buckets['Buckets']:
         bucket= i['Name']
-        
         bucketlist.append(bucket)
     #print(bucketlist)
     assert bucketlist!=[] 
     
 
+def test_getbuckets():
+    bucketlist=[]
+    buckets=s3.list_buckets()
+    for i in buckets['Buckets']:
+        bucket= i['Name']
+        
+        bucketlist.append(bucket)
+    #print(bucketlist)
+    assert bucketlist!=[] 
 
 
